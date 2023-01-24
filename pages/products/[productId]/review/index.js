ac3df3f40efc4e197d05d2df0f1afe842d5f4b39
alirazaDigiMark <aliraza@digimarkdevelopers.com>
2023-01-24 17:39:12 +0500

    product/1/review/23 routing

--- pages/products/[productId]/review/index.js
+++ pages/products/[productId]/review/index.js
@@ -1,7 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function index() {
   return (
+    <>
     <div>Product Review</div>
+    <Link href={"/products/1/review/23"}><li>Review 1st</li></Link>
+      <li>Review 2st</li>
+      <li>Review 3st</li>
+      </>
   )
 }
